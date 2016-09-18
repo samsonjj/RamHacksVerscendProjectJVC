e1f44307acbb47256ab7e13265d3a4bb61c50a60
sehgalv <sehgalv@vcu.edu>
2016-09-18 10:25:54 -0400

    Add files via upload
    updated transformations

--- Transformation.java
+++ Transformation.java
@@ -17,14 +17,14 @@ public class Transformation {
 		if (inputRecords[3] == "101" || inputRecords[3] == "110" || inputRecords[3] == "111" || inputRecords[3] == "201"
 				|| inputRecords[3] == "205" || inputRecords[3] == "206") {
 			inputRecords[3] = "CR";
-			System.out.println("Transformation to CR complete");
+//			System.out.println("Transformation to CR complete");
 		}
 		if (inputRecords[3] == "103" || inputRecords[3] == "104" || inputRecords[3] == "105" || inputRecords[3] == "107"
 				|| inputRecords[3] == "108" || inputRecords[3] == "109" || inputRecords[3] == "112"
 				|| inputRecords[3] == "113" || inputRecords[3] == "202" || inputRecords[3] == "203"
 				|| inputRecords[3] == "204") {
 			inputRecords[3] = "PA";
-			System.out.println("Transformation to PA complete");
+//			System.out.println("Transformation to PA complete");
 		}
 
 	}
@@ -33,15 +33,15 @@ public class Transformation {
 		if (inputRecords[2] == "HIP"){
 			inputRecords[20] = "INPT";
 			inputRecords[2] = "I";
-			System.out.println("Transformation to INPT and I complete");
+//			System.out.println("Transformation to INPT and I complete");
 		} else if (inputRecords[2] == "HOP"){
 			inputRecords[20] = "OUTP";
 			inputRecords[2] = "I";
-			System.out.println("Transformation to OUTP and I complete");
+//			System.out.println("Transformation to OUTP and I complete");
 		} else{
 			inputRecords[2] = "P";
 			inputRecords[20] = "PHYS";
-			System.out.println("Transformation to P and PHYS complete");
+//			System.out.println("Transformation to P and PHYS complete");
 		}
 	}
 	
@@ -50,19 +50,21 @@ public class Transformation {
 			inputRecords[34] = inputRecords[9];
 			inputRecords[35] = inputRecords[10];
 			inputRecords[36] = inputRecords[11];
-			inputRecords[75] = inputRecords[12];
+			inputRecords[76] = inputRecords[12];
 			inputRecords[37] = inputRecords[13];
 			inputRecords[38] = inputRecords[14];
 			inputRecords[39] = inputRecords[15];			
 			inputRecords[40] = inputRecords[16];
 			inputRecords[42] = inputRecords[17];
 			inputRecords[41] = inputRecords[18];
+//			System.out.println("copy from member to other sub");
 		}
 	}
 	
 	public void changeDateClaimPaid () {
 		if (!inputRecords[6].isEmpty()) {
-			inputRecords[33] = null;
+			inputRecords[33] = "null";
+//			System.out.println("change date claim paid");
 		}
 	}
 }
